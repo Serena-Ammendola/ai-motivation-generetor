8a0207722ffd971efcc76ec433cb3b6419894dfe
Serena-Ammendola <serena.ammendola95@gmail.com>
2024-04-03 23:54:17 +0100

    added basic js and typewriter

--- src/javascript.js
+++ src/javascript.js
@@ -1,1 +1,13 @@
+function generateQuote(event) {
+  event.preventDefault();
 
+  new Typewriter("#quotes", {
+    strings: "Your self worth is determined by you.",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let quoteFormElement = document.querySelector("#quote-generetor");
+quoteFormElement.addEventListener("submit", generateQuote);
